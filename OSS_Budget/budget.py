30606111f0bddbf119a2e10efa74ea5cc00109e8
김머현 <kdh031230@gmail.com>
2025-06-04 22:47:42 +0900

    [기능추가] 카테고리별 지출 합계 및 상세 조회 기능 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -20,6 +20,38 @@ class Budget:
             print(f"{idx}. {e}")
         print()
 
+    def category_detail(self, selected, summary):
+        if selected not in summary:
+                print("해당 카테고리가 없습니다. 다시 입력하세요.\n")
+                return
+        print(f"\n[{selected}] 카테고리 상세 내역")
+        for e in self.expenses:
+            if e.category == selected:
+                print(e)
+        print()         
+
+    def category_expenses(self):
+        if not self.expenses:
+            print("지출 내역이 없습니다.\n")
+            return
+
+        while True:
+            summary = {}
+            for e in self.expenses:
+                summary[e.category] = summary.get(e.category, 0) + e.amount
+
+            print("\n[카테고리별 지출 합계]")
+            for category, total in summary.items():
+                print(f"{category}: {total}원")
+            print()
+        
+            detailYN = input("상세 조회하려면 \"/F\", 메뉴로 돌아가려면 아무 키나 누르세요> ").strip()
+            if detailYN == "/F":
+                selected = input("상세 조회할 카테고리명을 입력하세요> ").strip()
+                self.category_detail(selected, summary)
+            else:
+                break
+
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -9,11 +9,12 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 카테고리별 지출 보기") #추가한 기능
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
-            category = input("카테고리 (예: 식비, 교통 등): ")
+            category = input("카테고리 (예: 식비, 교통 등): ").strip()
             description = input("설명: ")
             try:
                 amount = int(input("금액(원): "))
@@ -29,6 +30,9 @@ def main():
             budget.total_spent()
 
         elif choice == "4":
+            budget.category_expenses()
+
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
